c6f9db79f6f01e168961b3061ea39632c389fb43
Kasper van den Berg <kasper@kaspervandenberg.net>
2019-12-27 14:35:14 +0100

    Implement store and make-ctrie return a kb-ctrie

--- src/kb-ctrie.lisp
+++ src/kb-ctrie.lisp
@@ -2,17 +2,25 @@
   (:use :cl)
   (:export :make-ctrie
 	   :store
-	   :retrieve))
+	   :retrieve
+	   :is-kb-ctrie))
 (in-package :kb-ctrie)
 
 (defun make-ctrie ()
   "No code needed"
-  nil)
+  '(kb-ctrie))
 
 (defun store (ctrie value)
   "Store `value`"
-  (cons ctrie value))
+  (cons (car ctrie)
+	value))
 
 (defun retrieve (ctrie)
   "Retrieve a value previously store in `ctrie`"
   (cdr ctrie))
+
+(defun is-kb-ctrie (ctrie)
+  "Is `ctrie` a kb-ctrie? I.e. something one which `store` and `retrieve` can operate?"
+  (and (listp ctrie)
+       (eq (car ctrie)
+	   'kb-ctrie)))
