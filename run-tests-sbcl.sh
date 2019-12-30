64b8fdee023f84485b6b2533a71fcf8dc036eda4
Kasper van den Berg <kasper@kaspervandenberg.net>
2019-12-30 12:10:56 +0100

    Implement colorize test results to support solarized terminal color schema

--- run-tests-sbcl.sh
+++ run-tests-sbcl.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-sbcl --eval "\
+sbcl --noinform \
+     --eval "\
      (progn
           (ql:quickload :kb-ctrie)\
      	  (ql:quickload :kb-ctrie-test)\
 	  (asdf:test-system :kb-ctrie))"\
-    --quit	  
+     --quit \
+| sed -e 's/\x1b\[90m/\x1b\[92m/g'
