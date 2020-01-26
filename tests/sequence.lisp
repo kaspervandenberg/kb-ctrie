a74860559e864319b63d67bb25cd65280e41b3fe
Kasper van den Berg <kasper@kaspervandenberg.net>
2020-01-27 00:43:26 +0100

    Beforep compares any number of items

--- tests/sequence.lisp
+++ tests/sequence.lisp
@@ -1,11 +1,19 @@
 (in-package :kb-ctrie-test)
 
-(defun ≺ (item-a item-b sequence)
-    (beforep item-a item-b sequence))
-(defun beforep (item-a item-b sequence)
-  "Does `item-a` come before `item-b` in `sequence`"
-  (< (position item-a sequence)
-     (position item-b sequence)))
+(defun ≺ (&rest args)
+  "Syntax `(beforep item-1 [item-2 [item-3 [... [item-n]]]] sequence)`
+  Do the items come in order in `sequence`."
+    (apply #'beforep args))
+
+(defun beforep (&rest args)
+  "Syntax `(beforep item-1 [item-2 [item-3 [... [item-n]]]] sequence)`
+  Do the items come in order in `sequence`."
+  (when (>= (length args) 2)
+    (rest-arg-before-others args items seq
+      (flet ((pos-in-seq (x) (position x seq)))
+	(let ((positions (mapcar #'pos-in-seq items)))
+	  (and (not (some #'null positions))
+	       (apply #'< positions)))))))
 
 (defun all-permutations (sequence)
   "Give a list of all permutations of the elements in `sequence`"
@@ -17,3 +25,12 @@
 			     (cons item rest-permutations))
 			   (all-permutations (remove item sequence :test #'eq :count 1))))
 		 sequence))))
+
+(defmacro rest-arg-before-others (original-arg-values rest-arg-symbol fixed-arg-symbol &body body)
+  "Alter the order of arguments: the &rest argument comes first, after it come the required arguments.
+  The rest-symbol receives the remaining argument-values after the other symbols have received their values."
+  (let ((reversed-args-symbol (gensym "REVERSED-ARGS-")))
+    `(let ((,reversed-args-symbol (reverse  ,original-arg-values)))
+      (let ((,rest-arg-symbol (reverse (rest ,reversed-args-symbol)))
+	    (,fixed-arg-symbol (first ,reversed-args-symbol)))
+	,@body))))
